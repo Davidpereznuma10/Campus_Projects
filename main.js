b637170eab2ebfc3a948dc9211af9722150d3d64
David Pérez <jdpereznuma@gmail.com>
2023-03-13 20:14:13 -0500

    feat: :sparkles: Exersice 6 solution
    First i ask by the how many times repeat the cycle, after i ask the "Name", "Gender" and "Grade".after i make a table with the notes for each student, i create two variables to find the "min" and "max"  also   make two variables  to count the "Famales" and "Males" with logic parameters "|| OR" , and the last make a print with all paremeters asked previusly.

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-// Build the algorithm that reads two numbers from the keyboard,
-// If the first is greater than the second, report their sum and
-// difference, otherwise, inform the product and the
-// division of the first by the second.
-const n1 = parseFloat(prompt("Type the first number"));
-const n2 = parseFloat(prompt("Type the Second number"));
-
-if (n1 > n2) {
-  let add = n1 + n2;
-  let dif = n1 - n2;
-  console.log(`The Addition result is: ${add}, and the difference between the numbers is: ${dif}`);
-} else if (n1 < n2) {
-  let mul = n1 * n2;
-  let div = n1 / n2;
-  console.log(`The product result is ${mul}, and the division result is: ${div}`);
-} else {
-  console.log("The two numbers are the same");
-};
-
+// Build the algorithm in Javascript for a program
+// for any number of students reading the name,
+// the sex and the final grade and find the student with the highest
+// grade and the student with the lowest grade and how many were
+// men and how many women
+const num = parseFloat(prompt("How many students do you want to type?"));
+let Students = [];
+for (let i = 0; i < num; i++) {
+  let student = {
+    Name: prompt(`Type the student ${i + 1} name`),
+    Gender: prompt(`Type letter of the student ${i + 1} gender\n M: Male\n F: Female`),
+    Grade: parseFloat(prompt(`Type the student ${i + 1} grade`)),
+  };
+  Students.push(student);
+}
+console.table(Students);
+const grades = Students.map(item => item.Grade);
+const max = Math.max(...grades);
+const most = Students.find(function(element) {
+  return element.Grade === max;});
+const min = Math.min(...grades);
+const worst = Students.find(function(element) {
+  return element.Grade === min;});
+const genders = Students.map(item => item.Gender);
+const male = Students.filter(item => item.Gender.toLowerCase() === "m" || item.Gender.toLowerCase() === "male");
+const female = Students.filter(item => item.Gender.toLowerCase() === "f" || item.Gender.toLowerCase() === "female");
+console.log(`The student with the highest grade is ${most.Name}, and the lowest is ${worst.Name}.\nThere are ${male.length} males and ${female.length} females.`);
