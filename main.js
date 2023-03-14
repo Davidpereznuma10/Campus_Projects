4909393e10d416ef6f7cd6de30cd523772927c5c
David Pérez <jdpereznuma@gmail.com>
2023-03-13 22:48:31 -0500

    feat: :sparkles: Exercise 9 Solution
    First I mark the world record, i ask how many athletes wanna input,  after make a cicle with the parameter ask previusly, after make a funtion constructure to input the "Athlete" and "jump mark",  after make a  function  with a  "if"  to validate if the world record was break o not and finally make a print depend if was or not.

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-// Program that enters by keyboard:
-// a. the value of the side of a square to display on the screen the
-// perimeter of the same
-// b. the base and height of a rectangle to show the area of ​​the same
-const  side = parseFloat(prompt("Type the side of square"));
-const square = alert(`How the side measure is ${side}cm, and the parimeter measure is ${side*4}cm`)
+// N athletes have advanced to triple jump finals in the games
+// 2022 Women's Olympians. Design a program that asks for
+// keyboard the names of each finalist athlete and in turn, their
+// marks of the jump in meters. Inform the name of the athlete
+// champion to keep the gold medal and if she broke
+// Record, report the payment that will be 500 million. Register
+// It is at 15.50 meters.
+const recordtop = 15.50;
+const num = parseInt(prompt("How many athletes do you want to type?"));
+const olimpics = [];
 
-const base = parseFloat(prompt("Type the rectangle base"));
-const height = parseFloat(prompt("Type the rectangle height"));
-const rectangule = alert(`How the base measure is ${base}cm and the height measure is ${height}cm, so the Area is ${base*height}`)
+for (let i = 0; i < num; i++) {
+  const olimpic = {
+    Athlete: prompt(`Type the Athlete ${i + 1} Name`),
+    Mark: parseFloat(prompt(`Type the marks of athlete ${i + 1} on the jump in meters`))
+  };
+  olimpics.push(olimpic);
+};
+
+console.table(olimpics);
+
+const jumps = olimpics.map(item => item.Mark);
+const best = Math.max(...jumps);
+const record = olimpics.find(function(element) {
+  if (best >= recordtop) {
+    console.log(`Congrats you have broken the world record ${recordtop} mts, your mark was ${best} mts, you win the gold medal also you win 500 million.`);
+  } else {
+    console.log(`You have won the gold medal, your mark was ${best}`);
+  }
+});
