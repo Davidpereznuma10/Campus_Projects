71813047829fba2fcf7822917e26f6c2c69a41f1
David Pérez <jdpereznuma@gmail.com>
2023-03-13 21:47:03 -0500

    feat: :sparkles: Exercise 8 solution
    I ask by the parameters indicated to the "Rectangule and "Square"  and make an alert with the string

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-// Program that requests the entry of the name and price of an item and the
-// quantity carried by the customer. Show what the buyer must pay
-// on your bill.
-const inputArticle = prompt("Type the article");
-const inputCost = parseFloat(prompt("Type the price"));
-const inputAmount = parseFloat(prompt("Type the amount"));
+// Program that enters by keyboard:
+// a. the value of the side of a square to display on the screen the
+// perimeter of the same
+// b. the base and height of a rectangle to show the area of ​​the same
+const  side = parseFloat(prompt("Type the side of square"));
+const square = alert(`How the side measure is ${side}cm, and the parimeter measure is ${side*4}cm`)
 
-const totalCost = inputCost * inputAmount;
-console.log(`The price of ${inputArticle} is $${inputCost}.\nFor that reason your total cost is $${totalCost}`);
+const base = parseFloat(prompt("Type the rectangle base"));
+const height = parseFloat(prompt("Type the rectangle height"));
+const rectangule = alert(`How the base measure is ${base}cm and the height measure is ${height}cm, so the Area is ${base*height}`)
