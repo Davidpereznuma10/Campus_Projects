99b8176dcfdd47a8c9c67f1aeb9d3fc884ee4bc7
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-04-12 23:50:55 -0500

    feat: :rocket: Fix the funtions bugs
    Correct the marca, range and borrador mistake, and now they take de value select in the form.

--- main.js
+++ main.js
@@ -1,36 +1,43 @@
 const myForm = document.querySelector("#myForm");
-const myColor = document.querySelector("color")
+const myColor = document.querySelector("#color");
 const myRango = document.querySelector("#range");
 const valorRango = document.querySelector("#valor");
 const myMarca= document.querySelectorAll('input[name="marca"]');
-const myBorrador= document.querySelectorAll('input[name="borrador"]');
+const myBorrador = document.querySelector('input[name="borrador"]');
 const myMaterial= document.querySelectorAll('input[name="material"]');
+const No_Tiene = false;
+const Tiene = true;
 
 
 myRango.addEventListener('input', (e)=>{
-    const valor = myRango.value;
+    const valor = e.target.value;
     valorRango.textContent= `${valor} Cm`;
 });
 
+
 class lapiz{
-        #marca
-    constructor({ color="#FF0000",range=18,borrador= true,material="madera",}){
-    this.color = color;
-    this.range = range;
-    this.#marca = "Norma"; 
-    this.material = material;}
+    #marca
+    constructor({ color="#FF0000", range = 18, borrador = Tiene, material="madera", marca="Norma" }){
+        this.color = color;
+        this.range = range;
+        this.borrador= borrador;
+        this.#marca = marca;
+        this.material = material;
+    }
 
-    get marca(){
-        return this.#marca;}
+    getMarca(){return this.#marca;}
+    getRange(){return this.range;}
 
-    set marca(nuevaMarca){
-    this.#marca = nuevaMarca}
+    setMarca(nuevaMarca){this.#marca = nuevaMarca;}
+    setRange(nuevoRango){this.range = nuevoRango;}
 }
 
 const lectura = myForm.addEventListener('submit',(e)=>{
     e.preventDefault();
-    let formData = new FormData(myForm);
-    let formDataObj = Object.fromEntries(formData.entries());
-    let miLapiz = new lapiz(formDataObj)
+    const selectedMarca = document.querySelector('input[name="marca"]:checked');
+    const formData = new FormData(myForm);
+    const formDataObj = Object.fromEntries(formData.entries());
+    if (selectedMarca) {formDataObj.marca = selectedMarca.value;}
+    const miLapiz = new lapiz(formDataObj)
     console.log(miLapiz);
-})
+});
