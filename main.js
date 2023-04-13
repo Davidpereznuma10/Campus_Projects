07c2f554659ccaa0c8bbd0cff6900f3ce8a1ea18
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-04-12 22:51:32 -0500

    feat: :rocket: The funtions have been created
    Created all the funtion to make request it,  by now i gonna make the table print because the data is ready print like array in console.

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
+const myForm = document.querySelector("#myForm");
+const myColor = document.querySelector("color")
+const myRango = document.querySelector("#range");
+const valorRango = document.querySelector("#valor");
+const myMarca= document.querySelectorAll('input[name="marca"]');
+const myBorrador= document.querySelectorAll('input[name="borrador"]');
+const myMaterial= document.querySelectorAll('input[name="material"]');
+
+
+myRango.addEventListener('input', (e)=>{
+    const valor = myRango.value;
+    valorRango.textContent= `${valor} Cm`;
+});
+
 class lapiz{
-    #marca
-}newlapiz({color:"#FF0000",
-            })
+        #marca
+    constructor({ color="#FF0000",range=18,borrador= true,material="madera",}){
+    this.color = color;
+    this.range = range;
+    this.#marca = "Norma"; 
+    this.material = material;}
+
+    get marca(){
+        return this.#marca;}
+
+    set marca(nuevaMarca){
+    this.#marca = nuevaMarca}
+}
+
+const lectura = myForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let formData = new FormData(myForm);
+    let formDataObj = Object.fromEntries(formData.entries());
+    let miLapiz = new lapiz(formDataObj)
+    console.log(miLapiz);
+})
