9401e78b05ef383ed904ef4fd6ef1268ef3ad99f
aaa <aaa>
2023-04-13 09:37:34 -0500

    feat: :rocket: fix funtions
    fix the bugs and creted the funtion to preselect the option in html but material have a bug

--- main.js
+++ main.js
@@ -2,18 +2,14 @@ const myForm = document.querySelector("#myForm");
 const myColor = document.querySelector("#color");
 const myRango = document.querySelector("#range");
 const valorRango = document.querySelector("#valor");
-const myMarca= document.querySelectorAll('input[name="marca"]');
+const selectedMarca = document.querySelector('input[name="marca"]');
 const myBorrador = document.querySelector('input[name="borrador"]');
 const myMaterial= document.querySelectorAll('input[name="material"]');
 
 
-myRango.addEventListener('input', (e)=>{
-    const valor = e.target.value;
-    valorRango.textContent= `${valor} Cm`;
-});
 class lapiz{
     #marca
-    constructor({ color="#FF0000", range = 18 , borrador = true, material="madera", marca="Norma" }){
+    constructor({ color="#FF0000", range = 18 , borrador = true, material="Madera", marca="Norma" }){
         this.color = color;
         this.range = range;
         this.borrador= borrador;
@@ -22,16 +18,33 @@ class lapiz{
     }
 
     getMarca(){return this.#marca;}
-    getRange(){return this.range;}
-
     setMarca(nuevaMarca){this.#marca = nuevaMarca;}
-    setRange(nuevoRango){this.range = nuevoRango;}
 }
+
+
+window.addEventListener("DOMContentLoaded",() => {
+    const load = new lapiz({});
+    valorRango.textContent= `${myRango.value} Cm`;
+    myColor.value=  load.value;
+    selectedMarca.checked = load.getMarca();
+    console.log(myMaterial.checked = load.material);
+    myMaterial.checked = load.material;
+    myBorrador.checked = load.borrador;
+    console.log(load);
+    
+})
+
+
+myRango.addEventListener('input', (e)=>{
+    const valor = e.target.value;
+    valorRango.textContent= `${valor} Cm`;});
+
+
+
 
 
 const lectura = myForm.addEventListener('submit',(e)=>{
     e.preventDefault();
-    const selectedMarca = document.querySelector('input[name="marca"]:checked');
     const formData = new FormData(myForm);
     const formDataObj = Object.fromEntries(formData.entries());
     if (selectedMarca) {formDataObj.marca = selectedMarca.value;};
