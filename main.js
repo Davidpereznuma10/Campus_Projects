41c15678f6da083611782636821c2dd06d3a29bd
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-04-17 16:30:30 -0500

    feat: :art: fix the bugs aboout the video
    Make the funtion to acces the document after finished the video, also make that the table get the color that we choose ob the form.

--- main.js
+++ main.js
@@ -6,23 +6,18 @@ const valorRango = document.querySelector("#valor");
 const selectedMarca = document.querySelector('input[name="marca"]');
 const myBorrador = document.querySelector('input[name="borrador"]');
 const myMaterial= document.querySelectorAll('input[name="material"]');
+const loader = document.querySelector("#loader");
+const video = document.querySelector("#video");
 
-window.addEventListener("DOMContentLoaded", (e) => {
-    const animation = localStorage.getItem("animation");
-    const loader = document.getElementById("loader");
-    if (!animation) {
-      loader.style.display = "flex";
-      localStorage.setItem("animation", true);
-    }
-   setTimeout((e) => {
-      loader.style.display = "none";
-    }, 8000);
-  });
-
-
-  
-  
-
+video.src = 'style/img/logo_horizontal.mp4';
+video.play();
+video.addEventListener('ended',(e)=>{
+ video.style.display ='none';
+ loader.style.display='none';
+}) 
+window.addEventListener("DOMContentLoaded",(e)=>{
+  localStorage.getItem('loader') ? loader.style.display='none' : localStorage.setItem('loader', loader) ;
+})
 class lapiz{
     #marca
     constructor({ color="#FF0000", range = 18 , borrador = true, material="Madera", marca="Norma" }){
@@ -32,12 +27,9 @@ class lapiz{
         this.#marca = marca;
         this.material = material;
     }
-
     getMarca(){return this.#marca;}
     setMarca(nuevaMarca){this.#marca = nuevaMarca;}
 }
-
-
 window.addEventListener("DOMContentLoaded",() => {
     const load = new lapiz({});
     valorRango.textContent= `${myRango.value} Cm`;
@@ -49,16 +41,11 @@ window.addEventListener("DOMContentLoaded",() => {
         }
     });
 })
-
   let dataRango = myRango.addEventListener('input', (e)=>{
     const valor = e.target.value;
     valorRango.textContent= `${valor} Cm`;
     imagen.style.width = `${valor*0.8}Cm`;
 });
-
-
-
-
 const lectura = myForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     const selectedMarca = document.querySelector('input[name="marca"]:checked');
@@ -68,7 +55,7 @@ const lectura = myForm.addEventListener('submit',(e)=>{
     const miLapiz = new lapiz(formDataObj);
     const table = document.querySelector("#table").insertAdjacentHTML("beforeend",`
     <tr>
-            <th>${miLapiz.color}</th>
+            <th><input type="color" value="${miLapiz.color}"></th>
             <th>${miLapiz.range} Cm</th>
             <th>${miLapiz.getMarca()}</th>
             <th>${miLapiz.borrador}</th>
@@ -76,5 +63,3 @@ const lectura = myForm.addEventListener('submit',(e)=>{
     </tr>`);
     console.table(miLapiz);
 });
-
-
